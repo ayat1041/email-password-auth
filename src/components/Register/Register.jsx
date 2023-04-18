bd528b410fdd02ca8c7b154cd9d26f2cc3c33738
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-18 15:20:08 +0600

    register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,6 +4,13 @@ const Register = () => {
     return (
         <div>
             <h2>Register</h2>
+            <form>
+                <input type="email" name="email" id="email" placeholder="your email address"/>
+                <br />
+                <input type="password" name="password" id="password" placeholder='your password'/>
+                <br />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
